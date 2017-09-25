c2df6a29670a0d61246e8086b7b85daac664a2a7
Devonte <devo.tox.89@gmail.com>
2017-09-25 12:55:04 +0100

    Ember Roboto Font Face

--- index.js
+++ index.js
@@ -2,5 +2,21 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-roboto-fontface'
+	name: 'ember-cli-roboto-fontface',
+	options: {
+		nodeAssets: {
+			'roboto-fontface': {
+				srcDir: 'css/roboto',
+				import: ['roboto-fontface.css'],
+				public: {
+					srcDir: 'fonts',
+					destDir: 'fonts',
+					include: ['roboto/*']
+				}
+			}
+		}
+	},
+	included(/* app */) {
+		this._super.included.apply(this, arguments);
+	}
 };
